f49e69310d3a0b0f5d14b58e93a7368bf465ec38
zenglanting <1121085564@qq.com>
2021-09-27 15:46:44 +0800

    second commit

--- src/main/java/com/zero/git/GitTest01.java
+++ src/main/java/com/zero/git/GitTest01.java
@@ -3,5 +3,6 @@ package com.zero.git;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!22222222222222");
     }
 }
